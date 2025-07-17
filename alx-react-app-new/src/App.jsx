0a262579b5b2fd7a9fde35884e48aef7f5e80c86
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-07-17 07:45:52 +0100

    counter added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,9 +7,10 @@ import Header from './components/Header';
 import MainContent from './components/MainContent'; 
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -26,18 +27,19 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
        <Header /> 
        <MainContent />
+       <Counter />
        <Footer />. 
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+        </p> 
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
