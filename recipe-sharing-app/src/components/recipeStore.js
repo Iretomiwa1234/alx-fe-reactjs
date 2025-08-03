7a7f385f2de6d7e82d5767c442e40f58ddc60844
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-08-03 22:33:55 +0100

    messages

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,17 +1,54 @@
-// src/recipeStore.js
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => {
+      const updated = [...state.recipes, newRecipe];
+      return {
+        recipes: updated,
+        filteredRecipes: filterWithTerm(updated, state.searchTerm),
+      };
+    }),
+
   deleteRecipe: (id) =>
-    set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
+    set((state) => {
+      const updated = state.recipes.filter((r) => r.id !== id);
+      return {
+        recipes: updated,
+        filteredRecipes: filterWithTerm(updated, state.searchTerm),
+      };
+    }),
+
   updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((r) =>
+    set((state) => {
+      const updated = state.recipes.map((r) =>
         r.id === updatedRecipe.id ? updatedRecipe : r
-      ),
+      );
+      return {
+        recipes: updated,
+        filteredRecipes: filterWithTerm(updated, state.searchTerm),
+      };
+    }),
+
+  setSearchTerm: (term) =>
+    set((state) => ({
+      searchTerm: term,
+      filteredRecipes: filterWithTerm(state.recipes, term),
     })),
-  setRecipes: (recipes) => set({ recipes }),
+
+  setRecipes: (recipes) =>
+    set((state) => ({
+      recipes,
+      filteredRecipes: filterWithTerm(recipes, state.searchTerm),
+    })),
 }));
+
+function filterWithTerm(recipes, term) {
+  return recipes.filter((recipe) =>
+    recipe.title.toLowerCase().includes(term.toLowerCase())
+  );
+}
