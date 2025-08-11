5bc2e22532c29d3d91a40c2fa5d990962fd54478
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-08-11 23:49:58 +0100

    message

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail';
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Import your pages/components
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
@@ -13,4 +17,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
